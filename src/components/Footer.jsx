538b553154e1b1139fee0125a6513d88362675aa
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-11-16 23:28:25 +0600

    Improving UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-[#252525] font-semibold text-xs md:text-base text-center absolute bottom-3.5 select-none px-2.5'>
+    <div className='text-[#505050] font-semibold text-xs md:text-base text-center absolute bottom-3.5 select-none px-2.5'>
         copyright &copy; 2025 <a href='https://www.linkedin.com/in/md-al-baki-akon-352989362/' target='_blank' className='text-[#00C950]'> Md. Al Baki Akon</a>. All rights reserved. 
     </div>
   )
