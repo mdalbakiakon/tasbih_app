5c845f98b4fbde6db46cd925ce7bf9fcb2ca7e97
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-11-17 13:39:19 +0600

    Improving UI

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,28 @@ function App() {
   return (
     <>
       <div className="bg-[#121212] h-screen flex flex-col justify-center items-center relative p-5 font-outfit">
-        <button
-          onClick={() => setCount(0)}
-          className="text-center text-sm md:text-lg bg-[#303030] px-5 py-2.5 rounded-2xl font-semibold text-white cursor-pointer select-none"
-        >
-          Reset
-        </button>
+        <div className="flex flex-col justify-center items-center">
+          <button
+            onClick={() => setCount(0)}
+            className="text-center text-sm md:text-lg bg-[#303030] px-5 py-2.5 rounded-2xl font-semibold text-white cursor-pointer select-none"
+          >
+            Reset
+          </button>
 
-        <div className="text-[96px] py-2.5 md:py-0 leading-normal md:text-[120px] font-bold text-white select-none">
-          {count}
-        </div>
-        <div className="relative">
-          <Upperbtn
-            onClick={() => {
-              setCount(count + 1);
-            }}
-          />
-          <Lowerbtn
-            onClick={() => setCount((count) => Math.max(0, count - 1))}
-          />
+          <div className="text-[96px] py-2.5 md:py-0 leading-normal md:text-[120px] font-bold text-white select-none">
+            {count}
+          </div>
+
+          <div className="relative pb-15">
+            <Upperbtn
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            />
+            <Lowerbtn
+              onClick={() => setCount((count) => Math.max(0, count - 1))}
+            />
+          </div>
         </div>
         <Footer />
       </div>
