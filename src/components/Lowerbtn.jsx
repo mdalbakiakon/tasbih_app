d4ec0ff7bbf2c9105083d1d7fe04a042568b7219
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-11-17 12:59:22 +0600

    Improving UI with vibrant color

--- src/components/Lowerbtn.jsx
+++ src/components/Lowerbtn.jsx
@@ -5,7 +5,7 @@ const Lowerbtn = ({onClick}) => {
   return (
     <div 
         onClick={onClick}
-        className='w-[100px] aspect-square bg-[#445626] border-8 border-[#121212] rounded-full flex items-center justify-center cursor-pointer absolute -bottom-15 left-1/2 -translate-x-1/2 z-50 transition-all duration-300 ease-in-out active:bg-[#52682D]'>
+        className='w-[100px] aspect-square bg-[#445626] border-8 border-[#121212] rounded-full flex items-center justify-center cursor-pointer absolute -bottom-15 left-1/2 -translate-x-1/2 z-50 transition-all duration-300 ease-in-out active:bg-[#6E8B3D]'>
         <ChevronDown color='#96B85D' size={72} />
     </div>
   )
--- src/components/Upperbtn.jsx
+++ src/components/Upperbtn.jsx
@@ -5,7 +5,7 @@ const Upperbtn = ({ onClick }) => {
   return (
     <div
       onClick={onClick}
-      className=' w-[215px] md:w-[250px] aspect-square bg-[#445626] rounded-full flex items-center justify-center cursor-pointer z-40 transition-all duration-300 ease-in-out active:bg-[#52682D]'>
+      className=' w-[215px] md:w-[250px] aspect-square bg-[#445626] rounded-full flex items-center justify-center cursor-pointer z-40 transition-all duration-300 ease-in-out active:bg-[#6E8B3D]'>
       <ChevronUp color='#96B85D' size={126} />
     </div>
   )
