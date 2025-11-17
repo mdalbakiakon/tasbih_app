52b3b6cf9724ff5da6e2816e4b029f8aedc240f5
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-11-17 12:52:54 +0600

    Improving UI with vibrant color

--- src/components/Lowerbtn.jsx
+++ src/components/Lowerbtn.jsx
@@ -5,8 +5,8 @@ const Lowerbtn = ({onClick}) => {
   return (
     <div 
         onClick={onClick}
-        className='w-[100px] aspect-square bg-green-950 border-8 border-[#121212] rounded-full flex items-center justify-center cursor-pointer absolute -bottom-15 left-1/2 -translate-x-1/2 z-50 transition-all duration-300 ease-in-out active:bg-green-800'>
-        <ChevronDown color='#00C950' size={72} />
+        className='w-[100px] aspect-square bg-[#445626] border-8 border-[#121212] rounded-full flex items-center justify-center cursor-pointer absolute -bottom-15 left-1/2 -translate-x-1/2 z-50 transition-all duration-300 ease-in-out active:bg-[#52682D]'>
+        <ChevronDown color='#96B85D' size={72} />
     </div>
   )
 }
